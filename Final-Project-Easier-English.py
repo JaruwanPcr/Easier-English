939dc1a040e6b600bd378a6a1a45c82f72b2f426
JaruwanPcr <6540025222@student.chula.ac.th>
2023-12-11 23:18:55 +0700

    Update Final-Project-Easier-English.py

--- Final-Project-Easier-English.py
+++ Final-Project-Easier-English.py
@@ -38,29 +38,29 @@ st.markdown('Input a piece of text with challenging vocabulary, let the AI help 
 user_input = st.text_area("Enter text to simplify:", "Your text here")
 
 
-if st.button('Send'):
-    messages_so_far = [
-        {"role": "system", "content": prompt},
-        {'role': 'user', 'content': user_input},
-    ]
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages_so_far,
-        max_tokens=1000
-    )
+# if st.button('Send'):
+#     messages_so_far = [
+#         {"role": "system", "content": prompt},
+#         {'role': 'user', 'content': user_input},
+#     ]
+#     response = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         messages=messages_so_far,
+#         max_tokens=1000
+#     )
 
-    ai_response = response['choices'][0]['message']['content']
+#     ai_response = response['choices'][0]['message']['content']
 
-    simplified_text, definitions_part = ai_response.split('Definitions:', 1)
+#     simplified_text, definitions_part = ai_response.split('Definitions:', 1)
 
-    st.markdown('**Simplified Text:**')
-    st.write(simplified_text.strip())
+#     st.markdown('**Simplified Text:**')
+#     st.write(simplified_text.strip())
 
-    st.markdown('**Definitions:**')
-    for line in definitions_part.splitlines():
-        if line.startswith('['):
-            word, definition = line.split('] ', 1)
-            st.markdown(f"- **{word}]** {definition.strip()}")
+#     st.markdown('**Definitions:**')
+#     for line in definitions_part.splitlines():
+#         if line.startswith('['):
+#             word, definition = line.split('] ', 1)
+#             st.markdown(f"- **{word}]** {definition.strip()}")
 
 # if st.button('Send'):
 #     messages_so_far = [
@@ -85,3 +85,27 @@ if st.button('Send'):
 
 #     else:
 #         st.write(ai_response.strip())
+
+if st.button('Submit'):
+    messages_so_far = [
+        {"role": "system", "content": prompt},
+        {'role': 'user', 'content': user_input},
+    ]
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages_so_far,
+        max_tokens=1000
+    )
+
+    ai_response = response['choices'][0]['message']['content']
+
+    st.markdown('**Simplified Text:**')
+    if 'Definitions:' in ai_response:
+        simplified_text, definitions_part = ai_response.split('Definitions:', 1)
+
+        st.write(simplified_text.strip())
+
+        st.write(definitions_part.strip())
+
+    else:
+        st.write(ai_response.strip())
